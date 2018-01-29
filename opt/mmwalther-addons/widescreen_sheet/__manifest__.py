13558451345af82311535d69616e41c55114db98
Michael Walther <michaelhwalther@gmail.com>
2018-01-29 23:42:02 +0100

    [IMP] widescreen_sheet: Make the form sheet view use entire screen width

---
+++ opt/mmwalther-addons/widescreen_sheet/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Widescreen Sheet',
+    'version': '1.0',
+    'category': '',
+    "sequence": 14,
+    'complexity': "easy",
+    'category': 'Hidden',
+    'description': """
+        Make the form sheet view use entire screen width
+    """,
+    'author': 'Michael Walther',
+    'website': 'www.mmwalther.name',
+    'depends': ["base"],
+    'init_xml': [],
+    'data': [
+        "views/widescreen.xml",
+    ],
+    'demo_xml': [],
+    'test': [
+    ],
+    'qweb' : [
+        "static/src/xml/base.xml",
+    ],
+    'installable': True,
+#    'auto_install': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

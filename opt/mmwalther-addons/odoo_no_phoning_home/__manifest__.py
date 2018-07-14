c082c1df1b5409e0044e4d983387a5cdcb0b9402
Michael Walther <michaelhwalther@gmail.com>
2018-07-14 15:07:12 +0200

    [ADD] no_phoning_home is auto-install

--- opt/mmwalther-addons/odoo_no_phoning_home/__manifest__.py
+++ opt/mmwalther-addons/odoo_no_phoning_home/__manifest__.py
@@ -24,7 +24,7 @@
         "static/src/xml/base.xml",
     ],
     'installable': True,
-#    'auto_install': True,
-    'auto_install': False,
+    'auto_install': True,
+#    'auto_install': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

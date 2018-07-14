9f89a3a38daae51949be2eef969b9817c5ad8f59
Michael Walther <michael.walther@e-integration.de>
2018-07-14 14:18:03 +0200

    [ADD] add a parent package to be used for mmwalther modules

---
+++ opt/mmwalther-addons/mmwalther/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'MmWalther Addons',
+    'version': '1.0',
+    'author':   'Michael Walther',
+    'website' : 'https://www.mmwalther.name/odoo',
+    'category': 'Hidden/Dependency',
+    'depends' : ['base', ],
+    'description': """
+Mostly empty package to serve as the shared ancestor of all mmwalther modules.
+    """,
+    'data': [
+#        'security/analytic_security.xml',
+#        'security/ir.model.access.csv',
+#        'views/analytic_account_views.xml',
+    ],
+    'demo': [
+#        'data/analytic_demo.xml',
+#        'data/analytic_account_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
